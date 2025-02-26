6b6297c77cfe3d95352fceeaba2f3262d552139a
Rohit Mallikarjun <devoper.rohit@gmail.com>
2025-02-26 23:04:40 +0530

    RohhWorks | MHStays | Login and Logout | crud update

---
+++ services/login.js
@@ -0,0 +1,37 @@
+document.getElementById("loginForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    debugger
+    try {
+        const Localhost='http://localhost:21705/api/MHStays/';
+        const APIhost='https://rohhworks.bsite.net/api/MHStays/';
+        let BaseURl=Localhost;
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+   
+        const response = await fetch(BaseURl+"login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) { 
+            sessionStorage.setItem("clientTrust","yes")          
+            window.location.href = "dashboard.html"; // Redirect on success
+        } else {
+            sessionStorage.clear()
+            document.getElementById("errorMessage").innerText = data.message;
+        }
+    } catch (error) {
+        document.getElementById("errorMessage").innerText = "Login failed. Try again.";
+    }
+});
+
+function logout(event) {
+    debugger
+    event.preventDefault(); // Prevents the default anchor behavior (page reload)
+    sessionStorage.clear(); // Clear all session storage
+    window.location.href = "login.html"; // Redirect to login page
+}
