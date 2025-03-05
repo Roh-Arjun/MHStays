46abac535af35ab7b7fb156bd1c394dd6b5ae523
Rohit Mallikarjun <devoper.rohit@gmail.com>
2025-03-05 20:19:01 +0530

    RohhWorks | MHStays | update login

--- services/login.js
+++ services/login.js
@@ -1,33 +1,35 @@
-document.getElementById("loginForm").addEventListener("submit", async (event) => {
-    event.preventDefault();
-    debugger
-    try {
-        const Localhost='http://localhost:21705/api/MHStays/';
-        const APIhost='https://rohhworks.bsite.net/api/MHStays/';
-        let BaseURl=Localhost;
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("loginForm").addEventListener("submit", async (event) => {
+        event.preventDefault();
+        debugger;
+        try {
+            const Localhost = 'http://localhost:21705/api/MHStays/';
+            const APIhost = 'https://rohhworks.bsite.net/api/MHStays/';
+            let BaseURl = Localhost;
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
 
-   
-        const response = await fetch(BaseURl+"login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username, password })
-        });
+            const response = await fetch(BaseURl + "login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password })
+            });
 
-        const data = await response.json();
-
-        if (response.ok) { 
-            sessionStorage.setItem("clientTrust","yes")          
-            window.location.href = "dashboard.html"; // Redirect on success
-        } else {
-            sessionStorage.clear()
-            document.getElementById("errorMessage").innerText = data.message;
+            const data = await response.json();
+            console.log(data)
+            if (response.ok) {
+                sessionStorage.setItem("clientTrust", "yes")
+                window.location.href = "dashboard.html"; // Redirect on success
+            } else {
+                sessionStorage.clear()
+                document.getElementById("errorMessage").innerText = data.message;
+            }
+        } catch (error) {
+            document.getElementById("errorMessage").innerText = "Login failed. Try again.";
         }
-    } catch (error) {
-        document.getElementById("errorMessage").innerText = "Login failed. Try again.";
-    }
+    });
 });
+
 
 function logout(event) {
     debugger
